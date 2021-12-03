589572ac060ebc3aec01cf1fc71bf8d1f587b16f
Sutiksh Verma <verma.sutiksh27@gmail.com>
2021-12-03 15:01:42 -0500

    Added CSV Files
    Added
     - 5Lakh Hindi words csv file
     - 1M Hindi words csv file
     - 2M Hindi words csv file
     - 4M Hindi words csv file

--- UnicodeSorting/src/main/java/edu/neu/coe/info6205/sort/Unicode.java
+++ UnicodeSorting/src/main/java/edu/neu/coe/info6205/sort/Unicode.java
@@ -1,6 +1,7 @@
 package edu.neu.coe.info6205.sort;
 
-import edu.neu.coe.info6205.util.Benchmark_Timer;
+import
+        edu.neu.coe.info6205.util.Benchmark_Timer;
 import edu.neu.coe.info6205.util.FileUtil;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ import static edu.neu.coe.info6205.util.Utilities.show;
 public class Unicode {
 
     public static void main(String[] args) throws IOException {
-        List<String> words = FileUtil.hindiWordsList("UnicodeSorting/src/main/resources/modfixedwords.csv");
+        List<String> words = FileUtil.hindiWordsList("UnicodeSorting/src/main/resources/5LHindiWords.csv");
         Supplier<List<String>> supplier = () -> {
             Collections.shuffle(words);
             return words;
@@ -23,26 +24,26 @@ public class Unicode {
         String[] sortInput = words.toArray(new String[0]);
 
         //MSDStringSort Benchmark
-        Consumer<List<String>> msdSort = (x) -> MSD.sort(sortInput);
-        computeBenchMark(supplier, sortInput, msdSort, "MSDStringSort" + "- Randomly Ordered");
+//        Consumer<List<String>> msdSort = (x) -> MSD.sort(sortInput);
+//        computeBenchMark(supplier, sortInput, msdSort, "MSDStringSort" + "- Randomly Ordered");
         //Dual Pivot Quick Sort Benchmark
         Consumer<List<String>> quickDualPivotConsumer = (x) -> QuickDualPivot.sort(sortInput);
         computeBenchMark(supplier, sortInput, quickDualPivotConsumer, "QuickDualPivot" + "- Randomly Ordered");
 
         //Tim Sort Benchmark
-        TimSort timSort = new TimSort();
-        Consumer<List<String>> listConsumer = (x) -> timSort.sort(sortInput, 0, sortInput.length);
-        computeBenchMark(supplier, sortInput, listConsumer, "TimSort" + "- Randomly Ordered");
+//        TimSort timSort = new TimSort();
+//        Consumer<List<String>> listConsumer = (x) -> timSort.sort(sortInput, 0, sortInput.length);
+//        computeBenchMark(supplier, sortInput, listConsumer, "TimSort" + "- Randomly Ordered");
 
         //LSDStringSort Timer
-        Consumer<List<String>> lsdTimer = (x) -> LSDStringSort.sort(sortInput);
-        computeBenchMark(supplier, sortInput, lsdTimer, "LSDStringSort" + "- Randomly Ordered");
+//        Consumer<List<String>> lsdTimer = (x) -> LSDStringSort.sort(sortInput);
+//        computeBenchMark(supplier, sortInput, lsdTimer, "LSDStringSort" + "- Randomly Ordered");
     }
 
     private static void computeBenchMark(Supplier<List<String>> supplier, String[] sortInput, Consumer listConsumer, String description) {
         Benchmark_Timer<List<String>> benchmarkTimer = new Benchmark_Timer<>("Benchmark Test", null, listConsumer, null);
         double sortTime = benchmarkTimer.runFromSupplier(supplier, 100);
-//        show(sortInput, description);
+        show(sortInput, description);
         System.out.println(description + " Time Taken: " + sortTime + "ms");
     }
 }
