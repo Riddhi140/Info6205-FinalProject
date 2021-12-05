50b22477f7670869bcd521cde204c146c94dbf83
Daisy <daisy.v.quadros@gmail.com>
2021-12-04 21:12:58 -0500

    INFO-6205 refactoring

--- UnicodeSorting/src/main/java/edu/neu/coe/info6205/sort/Unicode.java
+++ UnicodeSorting/src/main/java/edu/neu/coe/info6205/sort/Unicode.java
@@ -31,7 +31,6 @@ public class Unicode {
 //                System.out.println("Array Size: " + arr.length);
                 return Arrays.asList(arr);
             };
-//            computeBenchMark(supplier, algoSort, description);
             double sortTime = benchmarkTimer.runFromSupplier(supplier, 10);
             System.out.println("Number of words: " + x + " " + description + " Time Taken: " + sortTime + "ms");
 
@@ -50,40 +49,27 @@ public class Unicode {
         //MSDStringSort Benchmark
         Consumer<List<String>> msdSort = (x) -> MSD.sort(sortInput);
         Unicode.sort(words, msdSort, "Order Situation - Randomly Ordered - " + "MSDStringSort");
-//        computeBenchMark(supplier, msdSort, "MSDStringSort" + "- Randomly Ordered");
 
         //Dual Pivot Quick Sort Benchmark
         Consumer<List<String>> quickDualPivotConsumer = (x) -> QuickDualPivot.sort(sortInput);
         Unicode.sort(words, quickDualPivotConsumer, "Order Situation - Randomly Ordered - " + "QuickSort");
-//
-////        computeBenchMark(supplier, sortInput, quickDualPivotConsumer, "QuickDualPivot" + "- Randomly Ordered");
-////
-////        //Tim Sort Benchmark
+
+        //Tim Sort Benchmark
        TimSort timSort = new TimSort();
         Consumer<List<String>> listConsumer = (x) -> timSort.sort(sortInput, 0, sortInput.length);
         Unicode.sort(words, listConsumer, "Order Situation - Randomly Ordered - " + "TimSort");
-////        computeBenchMark(supplier, sortInput, listConsumer, "TimSort" + "- Randomly Ordered");
-////
-////
-////        //LSDStringSort BenchMark
+
+        //LSDStringSort BenchMark
         Consumer<List<String>> lsdTimer = (x) -> LSDStringSort.sort(sortInput);
         Unicode.sort(words, lsdTimer, "Order Situation - Randomly Ordered - " + "LSDStringSort");
-//
-////        computeBenchMark(supplier, sortInput, lsdTimer, "LSDStringSort" + "- Randomly Ordered");
-////
-////        //HuskySort Benchmark
+
+        //HuskySort Benchmark
         MergeHuskySort huskySort =  new MergeHuskySort<>(HuskyCoderFactory.asciiCoder);
         Consumer<List<String>> huskySortConsumer = (x) -> huskySort.sort(sortInput);
         Unicode.sort(words, huskySortConsumer, "Order Situation - Randomly Ordered - " + "HuskySort");
 
-//        computeBenchMark(supplier, sortInput, huskySortConsumer, "Husky Sort" + "- Randomly Ordered");
 
     }
 
-//    private static void computeBenchMark(Supplier<List<String>> supplier, Consumer listConsumer, String description) {
-//        Benchmark_Timer<List<String>> benchmarkTimer = new Benchmark_Timer<>("Benchmark Test", null, listConsumer, null);
-//        double sortTime = benchmarkTimer.runFromSupplier(supplier, 20);
-////        show(sortInput, description);
-//        System.out.println(description + " Time Taken: " + sortTime + "ms");
-//    }
+
 }
